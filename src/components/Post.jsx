5a78be6857ea8dc6bafad6891d2e2ed0cc9de083
mohamim360 <mohamim360@gmail.com>
2023-02-20 14:25:32 +0600

    Styling & working with CSS Modules

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,14 @@
-
+import classes from "./Post.module.css";
 
 function Post(props) {
- 
-
   return (
-    <div>
-      <p>{props.author}</p>
-      <p>{props.text}</p>
+    <div className={classes.post}>
+      
+      <p className={classes.author}>{props.author}</p>
+      <div style={{ color: "red" }}>
+        text:
+      </div>
+      <p className={classes.text}>{props.text}</p>
     </div>
   );
 }
