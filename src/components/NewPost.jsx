dac5d01f83ce1fc59c29195a34cb34f47502b2a9
mohamim360 <mohamim360@gmail.com>
2023-02-20 16:06:24 +0600

    Adding & using “state” (via useState())

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,11 +1,19 @@
+import {useState} from 'react';
+
 import classes from './NewPost.module.css'
+
 function NewPost() {
+  const [newText,setNewText] = useState('');
+  function inputHandler(event){
+    setNewText(event.target.value);
+  }
   return (
   <form className={classes.form}>
    <p>
      <label htmlFor="body">Text</label>
-     <textarea id="body" required rows={3}/>
+     <textarea id="body" required rows={3} onChange={inputHandler}/>
    </p>
+   <p>{newText}</p>
    <p>
      <label htmlFor="name">Your Name</label>
      <input type="text" id="name" />
