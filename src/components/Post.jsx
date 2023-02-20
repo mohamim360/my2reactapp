ed4eb03197dae311b24f20a7ce6f71b8971c2657
mohamim360 <mohamim360@gmail.com>
2023-02-20 12:52:42 +0600

    Outputting dynamic values

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,14 @@
+const arrayName = ["hamim", "hasan"];
 
+function Post() {
+  const randomName = Math.random() > 0.5 ? arrayName[0] : arrayName[1];
 
-function Post(){
   return (
     <div>
-      <p>Hamim</p>
+      <p>{randomName}</p>
       <p>Lorem ipsum dolor sit amet.</p>
     </div>
-  )
+  );
 }
 
 export default Post;
