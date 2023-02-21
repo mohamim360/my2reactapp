c57af26c235d121bc3fe54e4299908d38523049a
mohamim360 <mohamim360@gmail.com>
2023-02-21 15:22:41 +0600

    change visibility of modal and backdrop in differant styles onclick

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,21 +1,20 @@
- 
-import classes from './Modal.module.css';
+import classes from "./Modal.module.css";
 import { useState } from "react";
 
-function Modal({children}){
+function Modal({ children }) {
   const [modalIsVisible, setModalIsVisible] = useState(true);
   function hideModalHandler(event) {
     setModalIsVisible(event.target.value);
   }
-return(
-  <>
-  <div className={classes.backdrop} onClick={hideModalHandler} />
-  
-  <dialog open={modalIsVisible} className={classes.modal}>
-   {children}
-  </dialog>
-  </>
- 
-)
+  return (
+    <>
+      {modalIsVisible && (
+        <div className={classes.backdrop} onClick={hideModalHandler} />
+      )}
+      <dialog open={modalIsVisible} className={classes.modal}>
+        {children}
+      </dialog>
+    </>
+  );
 }
 export default Modal;
